94c31ec75f3c04e9d03831bebbda304709a6a872
Armando Rios <armandorios.dev@gmail.com>
2025-06-21 15:24:02 -0700

    feat(ExploreSection): Add new section to display opportunities
    Introduce ExploreSection component to showcase mentorships and
    projects. Includes buttons for filtering and a loading state.
    Integrated into HomePage.

--- src/shared/pages/HomePage.tsx
+++ src/shared/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import HeroSection from '../components/homePage/HeroSection';
 import RolesSection from '../components/homePage/RolesSection';
 import ContactSection from '../components/homePage/ContactSection';
+import ExploreSection from '../components/homePage/ExploreSection';
 
 const HomePage = () => {
   return (
     <div className="flex-1">
       <HeroSection />
       <RolesSection />
+      <ExploreSection />
       <ContactSection />
     </div>
   );
