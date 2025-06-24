34c74aba18feba23a4a7264c8655d9c5f3fdaed8
Yatzaré Hernández <yatzarehernandez@gmail.com>
2025-06-24 18:37:07 -0500

    fix(dashboard): remove  inline sidebar

--- src/features/profiles/pages/DashboardPage.tsx
+++ src/features/profiles/pages/DashboardPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import useAuthStore from '../../auth/store/useAuthStore';
-import Sidebar from '../../../shared/components/SideBar';
 import axios from 'axios';
 
 const DashboardPage = () => {
@@ -13,16 +12,6 @@ const DashboardPage = () => {
     unreadMessages: 0,
     oportunidades: 0,
   });
-
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  useEffect(() => {
-    if (sidebarOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
-  }, [sidebarOpen]);
 
   useEffect(() => {
     if (isAuthenticated && user) {
@@ -45,52 +34,6 @@ const DashboardPage = () => {
   const {unreadMessages, oportunidades } = metrics;
 
   return (
-    <div className="w-screen h-screen flex flex-col overflow-x-hidden">
-
-      {/* Botón hamburguesa Responsive */}
-      <button
-        className="lg:hidden p-4"
-        onClick={() => setSidebarOpen(true)}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </button>
-
-      <div className="flex flex-1">
-        {/* Sidebar Desktop */}
-        <div className="hidden lg:flex">
-          <Sidebar
-          />
-        </div>
-
-        {/* Sidebar Responsive para tablet y móvil */}
-        {sidebarOpen && (
-          <div
-            className="fixed inset-0 bg-black bg-opacity-50 z-50"
-            onClick={() => setSidebarOpen(false)}
-          >
-            <div
-              className="bg-white w-64 h-full shadow-lg p-4 overflow-y-auto scrollbar-none"
-              onClick={(e) => e.stopPropagation()}
-            >
-              {/* Botón cerrar del botón de hamburguesa */}
-              <button
-                className="mb-4"
-                onClick={() => setSidebarOpen(false)}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-
-              <Sidebar
-              />
-            </div>
-          </div>
-        )}
-
-        {/* Contenido principal */}
         <main className="flex-1 bg-[#36393F4D] pt-4 overflow-y-auto">
           <div className="bg-[#FFFFFFF7] p-8 max-w-[1100px] w-full rounded-lg shadow-sm mx-auto mt-4">
             <h1 className="text-2xl font-bold text-[#16A34A] mb-2">¡Hola {user.fullName}!</h1>
@@ -111,8 +54,6 @@ const DashboardPage = () => {
             )}
           </div>
         </main>
-      </div>
-    </div>
   );
 };
 
